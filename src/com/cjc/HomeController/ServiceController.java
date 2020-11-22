df61c2c6555ef871e2ce388c40739988bbe8e692
priteshsavale2311 <74338954+priteshsavale2311@users.noreply.github.com>
2020-11-22 10:46:41 +0530

    Update ServiceController.java
    line 4 add

--- src/com/cjc/HomeController/ServiceController.java
+++ src/com/cjc/HomeController/ServiceController.java
@@ -11,6 +11,7 @@ public class ServiceController {
 			System.out.println("Class Management collection java project ");
 			System.out.println("Class Management collection java project another sop ");
 			System.out.println("Class Management collection java project another sop line 3 eclipse  ");
+			System.out.println("Class Management collection java project another sop line 4   ");
 			System.out.println("1. Add course");
 			System.out.println("2. Show course");
 			System.out.println("3. Add Faculty");
